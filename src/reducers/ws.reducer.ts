fc81d56fc10e4e8b11ffb9da8b6289eee95002a5
webgurucan <webgurucan@gmail.com>
2021-12-18 02:12:26 -0500

    format file

--- src/reducers/ws.reducer.ts
+++ src/reducers/ws.reducer.ts
@@ -1,7 +1,14 @@
-import { WS_OPEN, WS_DISCONNECTED, WS_AUTH, WS_UNAUTH, WS_CONNECT } from "@/actions/ws.actions";
+import {
+  WS_OPEN,
+  WS_DISCONNECTED,
+  WS_AUTH,
+  WS_UNAUTH,
+  WS_CONNECT,
+  WS_SEND,
+} from "@/actions/ws.actions";
 import { WebSocketKindStateEnum } from "@/constants/websocket.enums";
 import { WSReducerState } from "@/models/ws-reducer-state";
-import _set from 'lodash/set';
+import _set from "lodash/set";
 
 // const EMPTY_COLLECTION = {
 //   [WebSocketKindEnum.MARKET]: WebSocketKindStateEnum.IDLE,
@@ -10,7 +17,7 @@ import _set from 'lodash/set';
 
 const initialState: WSReducerState = {
   wsCollection: {},
-}
+};
 
 export const wsReducer = (state = initialState, action) => {
   // console.log('%c[ws reducer]', 'background: #222; color: #bada55', action.type, action.payload);
@@ -19,26 +26,34 @@ export const wsReducer = (state = initialState, action) => {
     case WS_CONNECT: {
       const { id } = action;
       const { wsCollection } = state;
-      console.log('[reducer WS_CONNECT] >>> id', id )
+      console.log("[reducer WS_CONNECT] >>> id", id);
 
-      if(
-        wsCollection[id] === WebSocketKindStateEnum.CONNECTING || 
+      if (
+        wsCollection[id] === WebSocketKindStateEnum.CONNECTING ||
         wsCollection[id] === WebSocketKindStateEnum.OPENED
       ) {
-       return state;
+        return state;
       }
-      
+
       return {
         ...state,
-        wsCollection: _set(state.wsCollection, [id], WebSocketKindStateEnum.CONNECTING)
+        wsCollection: _set(
+          state.wsCollection,
+          [id],
+          WebSocketKindStateEnum.CONNECTING
+        ),
       };
     }
     case WS_OPEN: {
       const { id } = action;
-      console.log('[WS_OPEN] >>> id', id )
+      console.log("[WS_OPEN] >>> id", id);
       return {
         ...state,
-        wsCollection: _set(state.wsCollection, [id], WebSocketKindStateEnum.OPENED)
+        wsCollection: _set(
+          state.wsCollection,
+          [id],
+          WebSocketKindStateEnum.OPENED
+        ),
       };
     }
     case WS_DISCONNECTED: {
@@ -46,24 +61,38 @@ export const wsReducer = (state = initialState, action) => {
 
       return {
         ...state,
-        wsCollection: _set(state.wsCollection, [id], WebSocketKindStateEnum.IDLE)
-      }
+        wsCollection: _set(
+          state.wsCollection,
+          [id],
+          WebSocketKindStateEnum.IDLE
+        ),
+      };
     }
     case WS_AUTH: {
       const { id } = action.payload;
 
       return {
         ...state,
-        wsCollection: _set(state.wsCollection, [id], WebSocketKindStateEnum.AUTHORIZED)
-      }
+        wsCollection: _set(
+          state.wsCollection,
+          [id],
+          WebSocketKindStateEnum.AUTHORIZED
+        ),
+      };
     }
     case WS_UNAUTH: {
       const { id } = action;
 
       return {
         ...state,
-        wsCollection: _set(state.wsCollection, [id], WebSocketKindStateEnum.OPENED)
-      }
+        wsCollection: _set(
+          state.wsCollection,
+          [id],
+          WebSocketKindStateEnum.OPENED
+        ),
+      };
+    }
+    case WS_SEND: {
     }
     default: {
       return state;
