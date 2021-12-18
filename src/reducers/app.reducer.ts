91b4f4a4faffbe94645ad8dff0214c7ef2aef6c8
webgurucan <webgurucan@gmail.com>
2021-12-18 04:10:04 -0500

    format file

--- src/reducers/app.reducer.ts
+++ src/reducers/app.reducer.ts
@@ -1,4 +1,9 @@
-import { CLOSE_ALL_MODALS, CLOSE_MODAL, OPEN_MODAL, UPDATE_WS_ENTRY } from "@/actions/app.actions";
+import {
+  CLOSE_ALL_MODALS,
+  CLOSE_MODAL,
+  OPEN_MODAL,
+  UPDATE_WS_ENTRY,
+} from "@/actions/app.actions";
 import { USER_LOGOUT_SUCCESS } from "@/actions/auth.actions";
 import { TICKER_INITIALIZED } from "@/actions/ticker.actions";
 import { riskWsUrl, testurl1 } from "@/config/config";
@@ -9,7 +14,7 @@ const initialState = {
   toasts: EMPTY_ARRAY,
   socketAddresses: [testurl1],
   adminRiskUrl: riskWsUrl,
-  isReady: false
+  isReady: false,
   //... profile or sth
 };
 
@@ -21,13 +26,13 @@ export const appReducer = (state = initialState, action) => {
       return {
         ...state,
         socketAddresses: [...state.socketAddresses, ...entries],
-      }
+      };
     }
     case TICKER_INITIALIZED: {
       return {
         ...state,
-        isReady: true
-      }
+        isReady: true,
+      };
     }
     case OPEN_MODAL: {
       const { component, id, props } = action.payload;
@@ -35,48 +40,51 @@ export const appReducer = (state = initialState, action) => {
       const modal = {
         id,
         component,
-        modalProps: props
+        modalProps: props,
       };
 
       let modals = [...state.modals];
 
-      const index = modals.findIndex(m => m.id === modal.id);
+      const index = modals.findIndex((m) => m.id === modal.id);
       if (!~index) {
         modals = [...modals, modal];
         return {
           ...state,
-          modals
-        }
+          modals,
+        };
       } else {
-        if (component !== modals[index].component || !shallowCompareObjects(props, modals[index].modalProps)) {
+        if (
+          component !== modals[index].component ||
+          !shallowCompareObjects(props, modals[index].modalProps)
+        ) {
           modals[index] = modal;
           return {
             ...state,
-            modals
-          }
+            modals,
+          };
         }
       }
 
       return {
         ...state,
-      }
+      };
     }
     case CLOSE_MODAL: {
-      const id = action.payload
-      const remainModals = state.modals.filter(modal => modal.id !== id);
+      const id = action.payload;
+      const remainModals = state.modals.filter((modal) => modal.id !== id);
       const isEmpty = remainModals.length === 0;
 
       return {
         ...state,
-        modals: isEmpty ? EMPTY_ARRAY : remainModals
-      }
+        modals: isEmpty ? EMPTY_ARRAY : remainModals,
+      };
     }
     case CLOSE_ALL_MODALS:
     case USER_LOGOUT_SUCCESS: {
       return {
         ...state,
-        modals: EMPTY_ARRAY
-      }
+        modals: EMPTY_ARRAY,
+      };
     }
     default:
       return state;
